b9d2ae0d0d13ca994c40ad9190eacadc9fd7ea50
Arsen_Valeev_SectionC <156844701+ArsenValeevSectionC@users.noreply.github.com>
2024-11-29 12:06:09 -0500

    fix cards

--- src/containers/work/Work.jsx
+++ src/containers/work/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Work.css";
 import { Card, MainText } from "../../components";
+import { Container } from "react-bootstrap";
 
 const Work = () => {
   const cardsData = [
@@ -11,7 +12,6 @@ const Work = () => {
       imageUrls: [
         "/images/Cargo/1.png",
         "/images/Cargo/2.png",
-        // "/images/Cargo/3.JPG",
         "/images/Cargo/4.png",
         "/images/Cargo/5.png",
         "/images/Cargo/6.png",
@@ -23,31 +23,33 @@ const Work = () => {
       description:
         "Developed website for Cargo company implementing Admin panel",
       imageUrls: [
-        "/images/Photographer/1.png", 
-        "/images/Photographer/2.png", 
-        "/images/Photographer/3.png", 
-        "/images/Photographer/4.png", 
-        "/images/Photographer/5.png", 
-        "/images/Photographer/6.png", 
-        
+        "/images/Photographer/1.png",
+        "/images/Photographer/2.png",
+        "/images/Photographer/3.png",
+        "/images/Photographer/4.png",
+        "/images/Photographer/5.png",
+        "/images/Photographer/6.png",
       ],
       projectLink: "https://ph.poliiiiiiiiina.crabdance.com",
     },
   ];
+
   return (
     <div>
       <MainText />
-      <div className="work-cards">
-        {cardsData.map((card, index) => (
-          <Card
-            key={index}
-            title={card.title}
-            description={card.description}
-            imageUrls={card.imageUrls}
-            projectLink={card.projectLink}
-          />
-        ))}
-      </div>
+      <Container className="work-cards-container">
+        <div className="work-cards">
+          {cardsData.map((card, index) => (
+            <Card
+              key={index}
+              title={card.title}
+              description={card.description}
+              imageUrls={card.imageUrls}
+              projectLink={card.projectLink}
+            />
+          ))}
+        </div>
+      </Container>
     </div>
   );
 };
