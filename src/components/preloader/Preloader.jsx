78b2af0ec3096a89cd364fe45e48066b4122ee5b
Arsen_Valeev_SectionC <156844701+ArsenValeevSectionC@users.noreply.github.com>
2024-12-02 15:52:10 -0500

    Update time, bug Fix

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -4,10 +4,24 @@ import "./Preloader.css";
 const Preloader = () => {
   return (
     <div className="preloader">
-      <h1 className="preloader-text">
-      {/* Arsen <span className="highlight">Valeev</span> is a... */}
-      Cup of <span className="highlight">Tea</span> and 🍩.
-      </h1>
+      <pre className="preloader-text">
+        <span className="highlight">class</span> 
+        <span className="class-name"> Developer </span> {"{"}
+        <br />
+        &nbsp;&nbsp;<span className="highlight">public:</span>
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;<span className="class-name">Developer</span>() {"{"}
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="key">name</span> = <span className="string">"Aspiring Intern"</span>;
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="key">currentStatus</span> = <span className="string">"Computer Programming Student at Humber Polytechnic"</span>;
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className="key">email</span> = <span className="string">"aspiring.intern@domain.com"</span>;
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;{"}"}
+        <br />
+        {"};"}
+      </pre>
     </div>
   );
 };
